a41f1ca1a494f66ba8ec87bf5e2e6e7d895a9d66
Craft Sound Lab <85714059+JhonatamFeria@users.noreply.github.com>
2025-04-09 16:17:05 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+# _*_ coding: utf-8 _*_
+import streamlit as st
+from PIL import Image
+
+
+
+
+
+imalogo = Image.open("ima\logo.png")
+st.set_page_config(page_title="App", page_icon=imalogo, layout="centered")
+
+
+
+
+
+
+
+
+st.title("App")
+
+
+
+        
+          
+        
+    
+
+
+
+
